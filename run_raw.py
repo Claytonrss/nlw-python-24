2e183bebd0065f46c2383c98914dd203c3b85bba
Clayton Rafael <clayton.rssouza@gmail.com>
2024-02-06 08:17:07 -0300

    feat: create function generate barcode

---
+++ run_raw.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from barcode import Code128
+from barcode.writer import ImageWriter
+
+app = Flask(__name__)
+
+@app.route('/create_tag', methods=['POST'])
+def create_tag():
+    body = request.json
+    product_code = body.get('product_code')
+    tag = Code128(product_code, writer=ImageWriter())
+    path_tag = f'{tag}'
+    tag.save(path_tag)
+
+    return jsonify({"tag_path": path_tag, "product_code": product_code})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
