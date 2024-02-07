8bee9e865f3b592791edd3ee42a75ac541192e1a
Clayton Rafael <clayton.rssouza@gmail.com>
2024-02-07 18:53:00 -0300

    feat: implementing View for TagCreator with Http types

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, request, jsonify
+from src.views.http_types.http_request import HttpRequest
+from src.views.tag_creator_view import TagCreatorView
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 
 def create_tags():
-    print(request.json)
-    return jsonify({ "status" : "success"}), 200
+    http_request = HttpRequest(body=request.json)
+    tag_creator_view = TagCreatorView()
+    response = tag_creator_view.validate_and_create(http_request)
+    return jsonify(response.body), response.status_code
---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,8 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        body = http_request.body
+        product_code = body['product_code']
+        return HttpResponse(status_code=200, body={"product_code" : product_code})
