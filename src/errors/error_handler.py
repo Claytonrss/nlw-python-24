918782a1b45e2f548af1792a3dbaa6d9ede7187d
Clayton Rafael <clayton.rssouza@gmail.com>
2024-02-08 08:55:21 -0300

    feat: implement validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,15 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handler_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(status_code=error.status_code,
+                        body={
+                            "errors": [{
+                                    "title": error.name,
+                                    "detail": error.message,
+                            }]
+                        })
     return HttpResponse(status_code=500,
                         body={
                             "errors": [{
--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
+from src.validators.tag_creator_validator import tag_creator_validator
 from src.errors.error_handler import handler_errors
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
@@ -9,6 +10,7 @@ tags_routes_bp = Blueprint('tags_routes', __name__)
 
 def create_tags():
     try:
+        tag_creator_validator(request)
         http_request = HttpRequest(body=request.json)
         tag_creator_view = TagCreatorView()
         response = tag_creator_view.validate_and_create(http_request)
